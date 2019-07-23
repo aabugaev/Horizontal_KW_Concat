a3f9b590215795084875109ebb0bf78da7585adf
Anton Bugaev <32480392+aabugaev@users.noreply.github.com>
2019-07-23 15:36:23 +0300

    Add files via upload

--- Horizontal_KW_Concat.py
+++ Horizontal_KW_Concat.py
@@ -12,10 +12,11 @@ def install(package):
 try:
 	import pandas as pd
 	import numpy as np
+	import xlrd
 except:
     install('pandas')
     install('numpy')
-
+    install('xlrd')
 
 import pandas as pd
 import numpy as np
